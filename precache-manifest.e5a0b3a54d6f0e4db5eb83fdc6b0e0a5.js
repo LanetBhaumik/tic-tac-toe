98bc11a84c1c21f866ac94ce3367e076b6ae174b
LanetBhaumik <bhaumikdhameliya.lanet@outlook.com>
2022-02-21 17:02:17 +0530

    Updates

---
+++ precache-manifest.e5a0b3a54d6f0e4db5eb83fdc6b0e0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d7f1c93c6bfc00d265566ae05fc6d1",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4b3473b24965e2f3b503",
+    "url": "/tic-tac-toe/static/css/main.bf0769c0.chunk.css"
+  },
+  {
+    "revision": "65e239c2d72fd9f410f6",
+    "url": "/tic-tac-toe/static/js/2.5409adca.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/tic-tac-toe/static/js/2.5409adca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe3c5e491c67fe230bc",
+    "url": "/tic-tac-toe/static/js/3.a835f798.chunk.js"
+  },
+  {
+    "revision": "4b3473b24965e2f3b503",
+    "url": "/tic-tac-toe/static/js/main.357e39b4.chunk.js"
+  },
+  {
+    "revision": "5ae6bf98a46f2eabec90",
+    "url": "/tic-tac-toe/static/js/runtime-main.40658acc.js"
+  }
+]);
